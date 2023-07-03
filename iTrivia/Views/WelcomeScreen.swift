d831f90cee32fce5bd7dde1a34841448acf174b6
swiftwithasror <swiftwithasror@gmail.com>
2023-07-03 11:29:10 +0500

    First commit

---
+++ iTrivia/Views/WelcomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreen.swift
+//  iTrivia
+//
+//  Created by Asror Aliqulov on 03/07/23.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct WelcomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreen()
+    }
+}
