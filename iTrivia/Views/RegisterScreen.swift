b4404e618c79bacee0800d40393c866910e1cc6e
swiftwithasror <swiftwithasror@gmail.com>
2023-07-03 12:31:00 +0500

    Welcome Screen Design

---
+++ iTrivia/Views/RegisterScreen.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterScreen.swift
+//  iTrivia
+//
+//  Created by Asror Aliqulov on 03/07/23.
+//
+
+import SwiftUI
+
+struct RegisterScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterScreen()
+    }
+}
--- iTrivia/Views/WelcomeScreen.swift
+++ iTrivia/Views/WelcomeScreen.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct WelcomeScreen: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            Text("iTrivia")
+                .fontWeight(.bold)
+                .font(.system(size: 40))
+            Spacer()
+            HStack {
+                Button("Sign Up") {
+                    
+                }
+                    .foregroundColor(.blue)
+                    .font(.system(size: 24))
+                    .padding(.horizontal)
+                Button("Log In") {
+                    
+                }
+                    .foregroundColor(.blue)
+                    .font(.system(size: 24))
+                    .padding(.horizontal)
+            }
+        }
+        .padding(.all)
     }
 }
 
