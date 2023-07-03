e401fcb267a84da5e1c79e3266f4732763471a30
swiftwithasror <swiftwithasror@gmail.com>
2023-07-03 11:21:55 +0500

    Initial Commit

---
+++ iTrivia/iTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTriviaApp.swift
+//  iTrivia
+//
+//  Created by Asror Aliqulov on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
